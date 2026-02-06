964ac6667b20a4d8f5e1be16766f935a9650087e
TxSn4x <txsn4x@gmail.com>
2026-02-06 20:35:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ import os
 from os import environ
 import logging
 from logging.handlers import RotatingFileHandler
+import re
+
+id_pattern = re.compile(r'^-?\d+$')
 
 # Recommended
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
@@ -17,56 +20,71 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "link")
 
-#Auto approve 
-CHAT_ID = [int(app_chat_id) if id_pattern.search(app_chat_id) else app_chat_id for app_chat_id in environ.get('CHAT_ID', '').split()] # dont change anything 
-TEXT = environ.get("APPROVED_WELCOME_TEXT", "<b>{mention},\n\nʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ᴛᴏ ᴊᴏɪɴ {title} ɪs ᴀᴘᴘʀᴏᴠᴇᴅ.\n\‣ ᴘᴏᴡᴇʀᴇᴅ ʙʏ @Codeflix_Bots</b>")
+# Auto approve 
+CHAT_ID = [
+    int(app_chat_id) if id_pattern.search(app_chat_id) else app_chat_id
+    for app_chat_id in environ.get('CHAT_ID', '').split()
+]  # dont change anything
+
+TEXT = environ.get(
+    "APPROVED_WELCOME_TEXT",
+    "<b>{mention},\n\nʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ᴛᴏ ᴊᴏɪɴ {title} ɪs ᴀᴘᴘʀᴏᴠᴇᴅ.\n\‣ ᴘᴏᴡᴇʀᴇᴅ ʙʏ @Codeflix_Bots</b>"
+)
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
 # Default
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "40"))
-#--- ---- ---- --- --- --- - -- -  - - - - - - - - - - - --  - -
 
 # Start pic
 START_PIC = "https://files.catbox.moe/jtiv0p.jpg"
 START_IMG = "https://files.catbox.moe/oaxs1q.jpg"
+
 # Messages
-START_MSG = os.environ.get("START_MESSAGE", "<b>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴀᴅᴠᴀɴᴄᴇᴅ ʟɪɴᴋs sʜᴀʀɪɴɢ ʙᴏᴛ. ᴡɪᴛʜ ᴛʜɪs ʙᴏᴛ, ʏᴏᴜ ᴄᴀɴ sʜᴀʀᴇ ʟɪɴᴋs ᴀɴᴅ ᴋᴇᴇᴘ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟs sᴀғᴇ ғʀᴏᴍ ᴄᴏᴘʏʀɪɢʜᴛ ɪssᴜᴇs.\n\n<blockquote>‣ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/aniweb_bots'>AɴɪWᴇʙ Bᴏᴛs</a></blockquote></b>")
-HELP = os.environ.get("HELP_MESSAGE", "<b><blockquote expandable>» Creator: <a href=https://t.me/proyato>Yato</a>\n» Our Community: <a href=https://t.me/aniweb_nexus>Flix Network</a>\n» Anime Channel: <a href=https://t.me/aniweb_shogunate>Anime Channel</a>\n» Ongoing Anime: <a href=https://t.me/aniweb_shogunate>Aniweb Shogunate</a>\n» Developer: <a href=https://t.me/aniweb_bots>Aniweb Bots</a></b>")
-ABOUT = os.environ.get("ABOUT_MESSAGE", "<b><blockquote expandable>This bot is developed by AɴɪWᴇʙ Bᴏᴛs (@Aniweb_Bots) to securely share Telegram channel links with temporary invite links, protecting your channels from copyright issues.</b>")
+START_MSG = os.environ.get(
+    "START_MESSAGE",
+    "<b>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴀᴅᴠᴀɴᴄᴇᴅ ʟɪɴᴋs sʜᴀʀɪɴɢ ʙᴏᴛ. ᴡɪᴛʜ ᴛʜɪs ʙᴏᴛ, ʏᴏᴜ ᴄᴀɴ sʜᴀʀᴇ ʟɪɴᴋs ᴀɴᴅ ᴋᴇᴇᴘ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟs sᴀғᴇ ғʀᴏᴍ ᴄᴏᴘʏʀɪɢʜᴛ ɪssᴜᴇs.\n\n<blockquote>‣ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/aniweb_bots'>AɴɪWᴇʙ Bᴏᴛs</a></blockquote></b>"
+)
+
+HELP = os.environ.get(
+    "HELP_MESSAGE",
+    "<b><blockquote expandable>» Creator: <a href=https://t.me/aniweb_bots>Aniweb Bots</a>\n» Our Community: <a href=https://t.me/aniweb_nexus> Network</a>\n» Anime Channel: <a href=https://t.me/aniweb_shogunate>Anime Channel</a>\n» Ongoing Anime: <a href=https://t.me/aniweb_shogunate>Aniweb Shogunate</a>\n» Developer: <a href=https://t.me/aniweb_bots>Aniweb Bots</a></b>"
+)
+
+ABOUT = os.environ.get(
+    "ABOUT_MESSAGE",
+    "<b><blockquote expandable>This bot is developed by AɴɪWᴇʙ Bᴏᴛs (@Aniweb_Bots) to securely share Telegram channel links with temporary invite links, protecting your channels from copyright issues.</b>"
+)
 
 ABOUT_TXT = """<b>›› ᴄᴏᴍᴍᴜɴɪᴛʏ: <a href='https://t.me/aniweb_shogunate'>𝖠ɴɪ𝖶ᴇʙ 𝖲ʜᴏɢᴜɴᴀᴛᴇ</a>
 <blockquote expandable>›› ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/aniweb_bots'>Cʟɪᴄᴋ ʜᴇʀᴇ</a>
 ›› ᴏᴡɴᴇʀ: <a href='https://t.me/aniweb_bots'>AɴɪWᴇʙ Bᴏᴛs</a>
-›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: @aniweb_bots</b></blockquote>""" # Bhosdiwalo agar developer me Yato ka username hataya to agli baar se koi repo public nhi krunga!!
+›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: @aniweb_bots</b></blockquote>"""
 
 CHANNELS_TXT = """<b>›› ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/aniweb_shogunate'>𝖠ɴɪ𝖶ᴇʙ 𝖲ʜᴏɢᴜɴᴀᴛᴇ</a>
 <blockquote expandable>›› ʙᴏᴛs: <a href='https://t.me/aniweb_bots'>AɴɪWᴇʙ Bᴏᴛs</a>
 ›› ᴀᴅᴜʟᴛ ᴄʜᴀɴɴᴇʟs: <a href='https://t.me/aniweb_hentai'>Cᴏʀɴʜᴜʙ</a>
 ›› ᴍᴀɴʜᴡᴀ ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/aniweb_manga'>AɴɪWᴇʙ Mᴀɴɢᴀ</a>
 ›› ᴄᴏᴍᴍᴜɴɪᴛʏ: <a href='https://t.me/Aniweb_network'>AɴɪWᴇʙ Nᴇᴛᴡᴏʀᴋ</a>
-›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: @aniweb_bots</b></blockquote>""" # Bhosdiwalo agar developer me Yato ka username hataya to agli baar se koi repo public nhi krunga!!
+›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: @aniweb_bots</b></blockquote>"""
 
-#--- ---- ---- --- --- --- - -- -  - - - - - - - - - - - --  - -
 # Default
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = " ғᴜᴄᴋ ʏᴏᴜ, ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ᴏᴡɴᴇʀ. ɢᴏ ᴀᴡᴀʏ, ʙɪᴛᴄʜ !"
 
 # Logging
 LOG_FILE_NAME = "AniwebLinks.txt"
-DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", "-1003813852588")) # Channel where user links are stored
-#--- ---- ---- --- --- --- - -- -  - - - - - - - - - - - --  - -
+DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", "-1003813852588"))
 
 try:
     ADMINS = []
-    for x in (os.environ.get("ADMINS", "1008989961").split()):
+    for x in os.environ.get("ADMINS", "1008989961").split():
         ADMINS.append(int(x))
 except ValueError:
     raise Exception("Your Admins list does not contain valid integers.")
 
 # Admin == OWNER_ID
 ADMINS.append(OWNER_ID)
-ADMINS.append()
-
+ADMINS.append(OWNER_ID)  
 
 logging.basicConfig(
     level=logging.INFO,
